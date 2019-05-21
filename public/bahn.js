68066e3ec1cb2b1fdb4ba464eaa06417100c86b8
snwt <sebastian.wendt@cgi.com>
2019-05-21 14:14:02 +0200

    layout with css

--- public/bahn.js
+++ public/bahn.js
@@ -1,13 +1,17 @@
 $(document).ready(function() {
+   // fill in date on page load
    $('#datum').val(new Date().toISOString());
 
 
    $("#sucheBahnhof").click(function(event) {
       var x = document.getElementById("station").value;
-      var url = "https://api.deutschebahn.com/freeplan/v1/location/" + x;
+      var url = "https://api.deutschebahn.com/fahrplan-plus/v1/location/" + x;
       $.ajax({
          url: url,
          dataType: 'json',
+         beforeSend: function (xhr) {
+            xhr.setRequestHeader('Authorization', 'Bearer 7210b3de5c12050b530d5cf53ec11398');
+         },
          success: function(data) {
             let dropdown = $('#station-list');
             dropdown.empty();
@@ -19,7 +23,8 @@ $(document).ready(function() {
             });
          },
          error: function(result) {
-            alert("Error" + result);
+            console.error(result);
+            alert("Error: " + result.statusText)
          }
       });
    });
@@ -29,28 +34,45 @@ $(document).ready(function() {
      var dt = $('#datum').val();
      //var dt = new Date().toISOString();
      //var url = "https://api.deutschebahn.com/freeplan/v1/arrivalBoard/" + id + "?date=2019-05-19T16%3A30";
-     var url = "https://api.deutschebahn.com/freeplan/v1/arrivalBoard/" + id + "?date=" + dt;
+     var url = "https://api.deutschebahn.com/fahrplan-plus/v1/arrivalBoard/" + id + "?date=" + dt;
      console.log("select value: " + url);
       $.ajax({
          url: url,
          dataType: 'json',
+         beforeSend: function (xhr) {
+            xhr.setRequestHeader('Authorization', 'Bearer 7210b3de5c12050b530d5cf53ec11398');
+         },
          success: function(data) {
-            $('#stage').html('<table style="width:100%">');
-            $('#stage').append('<tr><th>Name</th><th>Typ</><th>Bahnsteig</th><th>Uhrzeit</th><th>Von</th></tr>');
+            var thead = '<thead><tr><th>Name</th><th>Typ</><th>Bahnsteig</th><th>Uhrzeit</th><th>Von</th></tr></thead>';
+            var tbody = '<tbody>'
             $.each(data, function(i, item) {
-               $('#stage').append('<tr>'); 
-               $('#stage').append('<td>' + data[i].name + '</td>');
-               $('#stage').append('<td>' + data[i].type + '</td>');
-               $('#stage').append('<td>' + data[i].track + '</td>');
-               $('#stage').append('<td>' + data[i].dateTime + '</td>');
-               $('#stage').append('<td>' + data[i].origin + '</td>');
-               $('#stage').append('</tr>');
+               tbody += '<tr>';
+               tbody += '<td>' + data[i].name + '</td>';
+               tbody += '<td>' + data[i].type + '</td>';
+               tbody += '<td>' + data[i].track + '</td>';
+               tbody += '<td>' + data[i].dateTime + '</td>';
+               tbody += '<td>' + data[i].origin + '</td>';
+               /*tbody += '<td><a href="#details-output" details="' + data[i].detailsId + '" class="details">Details</a></td>';*/
+               tbody += '</tr>';
             });
-            $('#stage').append('</table>');
+            $('#stage').empty().append('<table>'+thead+tbody+'</table>');
+
+            $('.details').click(click_details); // register to dynamically added items
          },
          error: function(result) {
-            alert("Error" + result);
+            console.error(result);
+            alert("Error: " + result.statusText);
          }
       });
    });
+
+   var click_details = function(event)
+   {
+      var details = event.currentTarget.attributes.details.nodeValue;
+      console.log(details);
+      detailsId = details; // cut off '#details='
+      var url = "https://api.deutschebahn.com/freeplan/v1/journeyDetails/" + detailsId;
+      console.log("show details: " + url);
+      //$('#details-output')
+   }
 });
